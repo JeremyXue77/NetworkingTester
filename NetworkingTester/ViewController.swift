383a469e0408992bbf58c5a3a2ad6f1e2bf8716b
Jeremy Xue <gg831006@gmail.com>
2020-08-25 15:22:21 +0800

    Initial Commit

---
+++ NetworkingTester/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NetworkingTester
+//
+//  Created by Jeremy Xue on 2020/8/25.
+//  Copyright © 2020 JeremyXue. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
